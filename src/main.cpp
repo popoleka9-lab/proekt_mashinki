d47981da2d21d3cbaa04fe2be758bd405e99df2c
Dani <popoleka9@gmail.com>
2026-09-13 19:42:32 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <raylib.h>
+#include "ball.h"
+
+int main() 
+{
+    const Color darkGreen = {200, 10, 133, 255};
+    
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 600;
+    
+    Ball ball;
+    
+    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    SetTargetFPS(60);
+    
+    while (!WindowShouldClose())
+    {
+        ball.Update();
+        
+        BeginDrawing();
+            ClearBackground(darkGreen);
+            ball.Draw();
+        EndDrawing();
+    }
+    
+    CloseWindow();
+}
